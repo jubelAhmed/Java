f4a153f74e4548b387eac66ce483fad0dba73d52
jubelAhmed <jubel35-1648@gmail.com>
2017-11-26 01:01:21 +0600

    thirdly again commit

--- Player.java
+++ Player.java
@@ -3,10 +3,5 @@ public class Player{
     public static void main(String [] args){
 
         System.out.println("When player have own confident then he he will be owner");
-        statusPrint();
-    }
-
-    public static void stausPrint(){
-        System.out.println("This player is good player");
     }
 }
